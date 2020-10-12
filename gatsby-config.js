68b76d8d6f8e4a0ad4a7c69cdf316b8c7d83ea13
brandendupont <branden.dupont@gmail.com>
2020-10-11 23:31:32 -0500

    first push

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,10 @@
-require(`dotenv`).config()
+require(`dotenv`).config();
 
 module.exports = {
   siteMetadata: {
-    title: "Reflex",
-    description: "Starter for Reflex.",
+    title: "SAO 4th",
+    description:
+      "Our office is committed to transparency and accountability. Therefore, we annually release data on key decision-points and our performance.",
     siteUrl: process.env.SITE_URL || "http://localhost:8000",
   },
   plugins: [
@@ -16,4 +17,4 @@ module.exports = {
       },
     },
   ],
-}
+};
