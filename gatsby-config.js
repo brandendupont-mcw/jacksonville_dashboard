e69cdd6eb461b1e76d1e66eabe63819f15aed943
brandendupont <branden.dupont@gmail.com>
2020-12-14 17:13:37 -0600

    update site

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,7 +2,7 @@ require(`dotenv`).config();
 
 module.exports = {
   siteMetadata: {
-    title: "SAO4 Data Dashboard",
+    title: "SAO4",
     description:
       "Our office is committed to transparency and accountability. We will regularly release data on key decision-points and indicators of our performance. This effort is part of the Prosecutorial Performance Indicators (PPI), a national network of prosecutors and researchers working toward data-driven reforms.",
     siteUrl: process.env.SITE_URL || "https://localhost:8000",
