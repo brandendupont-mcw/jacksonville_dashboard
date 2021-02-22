a07ff16d088c00d11d593e40cfb6789d50f79e65
brandendupont <branden.dupont@gmail.com>
2021-02-21 22:41:11 -0600

    gtag test

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,29 +9,14 @@ module.exports = {
   },
   plugins: [
     {
-      resolve: `gatsby-plugin-google-gtag`,
+      resolve: "gatsby-plugin-google-gtag",
       options: {
-        // You can add multiple tracking ids and a pageview event will be fired for all of them.
-        trackingIds: [
-          "G-8Y4TN17ZC9", // Google Analytics / GA
-        ],
-        // This object gets passed directly to the gtag config command
-        // This config will be shared across all trackingIds
-        gtagConfig: {
-          anonymize_ip: true,
-          cookie_expires: 0,
-        },
-        // This object is used for configuration specific to this plugin
+        trackingIds: ["G-8Y4TN17ZC9"],
         pluginConfig: {
-          // Puts tracking script in the head instead of the body
           head: true,
-          // Setting this parameter is also optional
-          respectDNT: true,
         },
       },
     },
-  ],
-  plugins: [
     "@reflexjs/gatsby-theme-base",
     "@reflexjs/gatsby-theme-post",
     "@reflexjs/gatsby-theme-video",
